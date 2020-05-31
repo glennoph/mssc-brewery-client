8642f30501dfcaa7cccee8cd04d084b1b6ffe356
gopdyck <glennoph@gmail.com>
2020-05-31 08:43:31 -0700

    add customer methods and tests

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Source code in this repository is to support my on line courses:
 * [Spring Boot Microservices with Spring Cloud](https://www.udemy.com/spring-boot-microservices-with-spring-cloud-beginner-to-guru/?couponCode=GIT_HUB2)
+
+----
+## go log
+### branch: go-assignment-50
+* BreweryClient.java - add Customer (get, set, update, delete) methods
+* BreweryClientTest.java - add tests for Customer get, set, update, delete methods
--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -50,4 +51,39 @@ class BreweryClientTest {
     void testDeleteBeer() {
         client.deleteBeer(UUID.randomUUID());
     }
+
+    //go code below
+    @Test
+    void testGetCustomerById() {
+        // given customer dto
+        CustomerDto dto = client.getCustomerById(UUID.randomUUID());
+        // assert customer dto not null
+        assertNotNull(dto);
+    }
+
+    @Test
+    void testSaveNewCustomer() {
+        // given new customer dto with name Bud
+        CustomerDto customerDto =  CustomerDto.builder().name("Bud").build();
+        URI customerUri = client.saveNewCustomer(customerDto);
+        assertNotNull(customerUri);
+        System.out.println(customerUri);
+    }
+
+
+    @Test
+    void testUpdateCustomer() {
+        // given new customer dto with name Sam
+        CustomerDto customerDto =  CustomerDto.builder().name("Sam").build();
+        client.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void testDeleteCustomer() {
+        // given customer dto
+        client.deleteCustomer(UUID.randomUUID());
+    }
+
+
+
 }
